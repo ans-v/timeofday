f958b364a238b45fad3d42736b09cfd3de710068
ansva <ansva@LAPTOP-097U31QI.lan>
2020-02-22 16:28:56 +0100

    mutable TimeOfDay

---
+++ timeofday/src/timeofday/TimeOfDay.java
@@ -0,0 +1,64 @@
+package timeofday;
+
+/**
+ * Instances of this class store a time of day.
+ * 
+ * @invar The hours are between 0 and 23
+ * 		| 0 <= getHours() && getHours() <= 23
+ * @invar The minutes are between 0 and 59 
+ * 		| 0 <= getMinutes() && getMinutes() <= 59
+ * 
+ */
+
+public class TimeOfDay {
+	
+	/* This is a private variant in the body of the class */
+	
+	/**
+	 * @invar minutesSinceMidnight is between 0 (inclusive) and 24*60 (exclusive)
+	 * 		| 0<= minutesSinceMidnight && minutesSinceMidnight <24*60
+	 * 
+	 */
+	
+	private int minutesSinceMidnight;
+	
+	public int getHours() {
+		return minutesSinceMidnight / 60; 
+		}
+	public int getMinutes() {
+		return minutesSinceMidnight % 60; 
+		}  /*geeft minuten huidig uur , x%y geeft rest*/
+
+	
+	
+	/**
+	 *Sets this object's hours.
+	 * 
+	 * @pre The given hours are between 0 and 23
+	 * 		| 0 <= hours && hours <= 23
+	 * @post This object's hours equal the given hours
+	 * 		|getHours() == hours
+	 * @post This object's minutes have remained unchanged
+	 * 		|getMinutes() == old(getMinutes())
+	 */
+	
+	public void setHours(int hours) {
+		minutesSinceMidnight = getMinutes() + hours * 60; 
+	}
+	
+	/**
+	 * Sets this object's minutes.
+	 * 
+	 * @pre The given minutes are between 0 and 59
+	 * 		| 0 <= minutes && minutes <= 59
+	 * @post This object's minutes equal the given minutes
+	 * 		|getMinutes() == minutes
+	 * @post This object's hours have remained unchanged
+	 * 		|getMinutes() == old(getHours())
+	 */
+	
+	public void setMinutes(int minutes) {
+		minutesSinceMidnight = getHours() * 60 + minutes; 
+	}
+
+}
